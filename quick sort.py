716b0b584e0b407ee4874944fa18385abdad7941
syed shouzeb hassan <38203938+shouzeb@users.noreply.github.com>
2020-02-05 22:06:09 +0500

    Add files via upload

---
+++ quick sort.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 21:55:27 2020
+
+@author: Shouzeb
+"""
+
+def partitionOfArray(array, beginPoint, endPoint):
+    pivotPoint = beginPoint
+    for i in range(beginPoint+1, endPoint+1):
+        if array[i] <= array[beginPoint]:
+            pivotPoint += 1
+            array[i], array[pivotPoint] = array[pivotPoint], array[i]
+    array[pivotPoint], array[beginPoint] = array[beginPoint], array[pivotPoint]
+    return pivotPoint
+
+
+
+def quicksortOfArray(array, beginPoint=0, endPoint=None):
+    if endPoint is None:
+        endPoint = len(array) - 1
+    def _quicksort(array, beginPoint, endPoint):
+        if beginPoint >= endPoint:
+            return
+        pivotPoint = partitionOfArray(array, beginPoint, endPoint)
+        _quicksort(array, beginPoint, pivotPoint-1)
+        _quicksort(array, pivotPoint+1, endPoint)
+    return _quicksort(array, beginPoint, endPoint)
+
+array = [97, 200, 100, 101, 211, 107]
+quicksortOfArray(array)
+print(array)
