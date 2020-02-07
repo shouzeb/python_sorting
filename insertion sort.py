2c76f4b22f2da71aa17397bf712dbfe149b018fd
syed shouzeb hassan <38203938+shouzeb@users.noreply.github.com>
2020-02-07 22:01:51 +0500

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 21:44:52 2020
+
+@author: Shouzeb
+"""
+
+# Insertion Sort In Python
+#
+# Performance Complexity = O(n^2)
+# Space Complexity = O(n)
+
+def insertionSortFunction(array):
+    # for every element in our array
+    for index in range(1, len(array)):
+        current = array[index]
+        position = index
+
+        while position > 0 and array[position-1] > current:
+            print("Swapped {} for {}".format(array[position], array[position-1]))
+            array[position] = array[position-1]
+            print(array)
+            position -= 1
+
+        array[position] = current
+
+    return array
+
+array = [8,2,1,3,5,4180,0,150,80]
+
+print(insertionSortFunction(array))
